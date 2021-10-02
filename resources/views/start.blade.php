92c1f544bd21c7100d55e9cab494dacab1548971
Laurent Perrier <laurent@simpleinvoices.io>
2021-10-02 01:01:30 -0400

    equipment

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -6,8 +6,8 @@
 		<p class="mt-6">We're excited to have you on the show!</p>
 		<p class="mt-4">Here's some information to help you sound great, and make the best of your experience.</p>
 		
-		<a href="/equipment" class="block mt-6 underline">1. Required Equipement</a>
-		<a href="/expect" class="block mt-6 underline">2. What To Expect</a>
+		<a href="/expect" class="block mt-6 underline">1. What To Expect</a>
+		<a href="/equipment" class="block mt-6 underline">2. Required Equipement</a>
 		<a href="/prepare" class="block mt-6 underline">3. How To Prepare</a>
 	</div>
 @endsection
