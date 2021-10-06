7f52732664cb5b1156c881a87c4383bfb3fed0aa
Laurent Perrier <laurent@simpleinvoices.io>
2021-10-05 22:07:55 -0400

    embed trailer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
 	<p class="lg:max-w-lg lg:text-xl max-w-full mt-12 text-lg text-left">
 		Every Monday, hosts <a href="https://www.instagram.com/vana_cristina/" target="_blank" class="underline">Vana</a> & <a href="https://www.instagram.com/itslaurentbtw/" target="_blank" class="underline">Laurent</a> send single men and women on audio dates where they try to find love (or at least slide in someone's DMs!)
 	</p>
-	<p class="lg:max-w-lg lg:text-xl max-w-full mt-6 text-lg text-left">
-		Coming soon!
-	</p>
+	<div class="mt-12">
+		<iframe src="https://anchor.fm/sayitbackpodcast/embed/episodes/Trailer-e18c33o" height="102px" width="100%" frameborder="0" scrolling="no"></iframe>
+	</div>
 </div>
 @endsection
