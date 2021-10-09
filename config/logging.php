500a846a40a2e193a967dd44f9d11d6866eeb0d6
Laurent Perrier <laurent@simpleinvoices.io>
2021-10-09 16:26:20 -0400

    bugsnag

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'bugsnag'],
             'ignore_exceptions' => false,
         ],
 
@@ -46,6 +46,10 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
+
+				'bugsnag' => [
+					'driver' => 'bugsnag',
+				],
 
         'daily' => [
             'driver' => 'daily',
